6e76412a80d48830855531d8f96aabde5f8dc856
kiam <kiam.krause@gmail.com>
2021-12-21 15:49:43 +0100

    ACII und Char Typ

---
+++ Udemy_C_Template/Chapter2_Variables/ContentChapter2/Buchstaben.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    char character_a = 'a';
+
+    printf("Character acii code: %d\n",character_a);
+    printf("Character: %c\n",character_a);
+
+    char character_z = character_a + 25;
+    printf("Character acii code: %d\n",character_z);
+    printf("Character: %c\n",character_z);
+
+    return 0;
+}
