4b1abe8d6e7678fdd1a52a2f1b705d3d15a19eb3
kiam <kiam.krause@gmail.com>
2021-12-23 16:46:10 +0100

    Übung For Schleife mit If

--- Udemy_C_Template/Chapter4_Loops/ContentChapter4/ExerciseChapter4.c
+++ Udemy_C_Template/Chapter4_Loops/ContentChapter4/ExerciseChapter4.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+
 int main()
 {
+
+    printf("<\t^\t^\t^\t^\t>\n");
+    for (int i = 1; i <= 4; i++){
+        printf("<     ");
+        for (int k = 1; k <=4; k++){
+            
+            if(k == 4){
+                printf("(%d,%d)\t>\n",i,k);
+            }
+            else{
+                printf("(%d,%d)   ",i,k);
+               
+            }
+        }
+    }
+    printf("<\tv\tv\tv\tv\t>\n");
 
     return 0;
 }
